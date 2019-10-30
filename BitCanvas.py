730ca2a03e3210730da8729d1d9b301b705b2e83
Chris Pecoraro <pecor016@umn.edu>
2019-10-30 23:10:05 +0000

    added custom buttons and fixed drawing/erasing/recent color bugs

--- BitCanvas.py
+++ BitCanvas.py
@@ -5,17 +5,19 @@ import math
 
 class BitCanvas():
     BACKGROUND_COLOR = 'white'
-    DEFAULT_COLOR = 'black'
+    DEFAULT_COLOR = '#000000'
     CANVAS_WIDTH = 500
     CANVAS_HEIGHT = 400
     gridDim = 24
+    undoStack = []
+    redoStack = []
 
 
     def init(self, tab):
         #--------------------main canvas---------------------
         self.canvas = Canvas(tab, width = self.CANVAS_WIDTH, height = self.CANVAS_HEIGHT)
         self.canvas.configure(background=self.BACKGROUND_COLOR)
-        self.canvas.pack()
+        self.canvas.pack(side=LEFT)
 
         self.canvas.bind('<Button-1>', self.drawLine)
         self.canvas.bind('<B1-Motion>', self.drawLine)
@@ -28,8 +30,6 @@ class BitCanvas():
         self.color = self.DEFAULT_COLOR
 
         #--------------------eraser active---------------------
-        self.eraser = False
-        self.undoStack = []
 
     def drawLine(self, event):
         #draws square based on clock event and grid
@@ -37,19 +37,32 @@ class BitCanvas():
         bottom_y = math.floor(event.y/self.CANVAS_HEIGHT*self.gridDim)/self.gridDim* self.CANVAS_HEIGHT
         top_x = math.ceil(event.x/self.CANVAS_WIDTH*self.gridDim)/self.gridDim* self.CANVAS_WIDTH
         top_y = math.ceil(event.y/self.CANVAS_HEIGHT*self.gridDim)/self.gridDim* self.CANVAS_HEIGHT
+
+        #if out of canvas bounds, dont draw
+        if bottom_x < 0 or bottom_y < 0 or top_x > self.CANVAS_WIDTH or top_y > self.CANVAS_HEIGHT:
+            return
+        #if square of same color is already there, dont draw
+        for i in self.undoStack:
+            if bottom_x >= self.canvas.bbox(i[0])[0] and top_x <= self.canvas.bbox(i[0])[2] and bottom_y >= self.canvas.bbox(i[0])[1] and top_y <= self.canvas.bbox(i[0])[3]:
+                if (i[1] == self.color):
+                    return
+                else:
+                    self.canvas.delete(i)
+                    self.undoStack.remove(i)
+        #draw square and add to undo stack
         x = self.canvas.create_rectangle(bottom_x, bottom_y, top_x, top_y, outline="",  fill = self.color)
-        self.undoStack.append(x)
+        self.undoStack.append((x, self.color))
         self.canvas.tag_raise('grid')
 
 
     def drawGrid(self):
         #top to bottom lines
-        for i in range(1,self.gridDim):
+        for i in range(0,self.gridDim):
             self.canvas.create_line(0, self.CANVAS_HEIGHT/self.gridDim*i, self.CANVAS_WIDTH, self.CANVAS_HEIGHT/self.gridDim*i,
                                width=1, fill='black',
                                capstyle=ROUND, smooth=TRUE, splinesteps=36, tags = 'grid')
         #left to right lines
-        for i in range(1,self.gridDim):
+        for i in range(0,self.gridDim):
             self.canvas.create_line(self.CANVAS_WIDTH/self.gridDim*i, 0, self.CANVAS_WIDTH/self.gridDim*i,self.CANVAS_HEIGHT,
                                width=1, fill='black',
                                capstyle=ROUND, smooth=TRUE, splinesteps=36, tags='grid')
@@ -58,12 +71,8 @@ class BitCanvas():
         self.canvas.delete('grid')
 
     def setColor(self, color):
-        if color == "White":
-            eraser = True
-            self.color = "White"
-        else:
-            eraser = False
-            self.color = color
+        self.color = color
+
     def getColor(self):
         return self.color
 
@@ -72,8 +81,12 @@ class BitCanvas():
 
     def clear(self, gridOn):
         self.canvas.delete('all')
+        self.undoStack = []
         if gridOn==1:
             self.drawGrid()
-    def undo():
-        x = self.stack.pop()
-        self.canvas.delete(x)
+
+    def undo(self):
+        if self.undoStack == []:
+            return
+        x = self.undoStack.pop()
+        self.canvas.delete(x[0])
--- Paint.py
+++ Paint.py
@@ -8,23 +8,28 @@ import os
 from PIL import Image, ImageTk
 
 class Paint():
-    DEFAULT_COLOR = 'Black'
+    DEFAULT_COLOR = '#000000'
     BACKGROUND_COLOR = '#afeeee'
 
     def init(self):
-        self.filepath = "C:/Users/Chris/Documents/Paint/images/untitled"
+        self.filepath = "C:/Users/Chris/Documents/Paint/images/untitled.jpg"
         self.filenameGiven = False
+        self.filetype = "JPEG"
+
  #--------------------main window---------------------
         self.window = Tk()
         self.window.title("Pixel Paint")
         self.window.geometry('600x550')
         self.window.configure(background=self.BACKGROUND_COLOR)
-        self.window.columnconfigure(0, weight=1)
 
 
         titlelabel = Label(self.window, text="Pixel Paint", foreground="black", background = 'white', font = ("Fixedsys", 24))
-        titlelabel.grid(row=0, column=0, sticky='ew')
-        titlelabel.configure(anchor=CENTER)
+        titlelabel.pack(side = "top", fill = "both")
+
+        self.top_frame = Frame(self.window, background = "light gray", highlightthickness = 1, highlightbackground="#E6DCDA")
+        self.bottom_frame = Frame(self.window, background = self.BACKGROUND_COLOR)
+        self.top_frame.pack(side="top", fill="x", expand=False)
+        self.bottom_frame.pack(side="bottom", fill="both", expand=True)
  #--------------------File, edit, ect---------------------
         self.menubar = Menu(self.window)
 
@@ -33,6 +38,7 @@ class Paint():
         self.file.add_command(label='New')
         self.file.add_command(label="Open")
         self.file.add_command(label="Save", command = self.save)
+        self.window.bind_all("<Control-s>", self.save)
         self.file.add_command(label="Save as", command = self.saveAs)
         self.file.add_separator()
         self.file.add_command(label="Exit")
@@ -40,70 +46,75 @@ class Paint():
         self.window.config(menu=self.menubar)
 
         self.edit=Menu(self.menubar,tearoff=0)
-        self.edit.add_command(label="Undo")
-        self.edit.add_command(label="Copy")
-        self.edit.add_command(label="Paste")
+        self.edit.add_command(label="Undo", command = self.undo)
+        self.window.bind_all("<Control-z>", self.undo)
         self.menubar.add_cascade(label="Edit", menu=self.edit)
-
         self.help=Menu(self.menubar,tearoff=0)
         self.help.add_command(label="Help")
         self.menubar.add_cascade(label="Help",menu=self.help)
 
- #--------------------Top Frame---------------------
-        self.top_frame = Frame(self.window)
-        self.top_frame.grid(column=0, row=1)
-
  #--------------------color selecting tool---------------------
-        self.color_selector = Button(self.top_frame, text='Select Color', command= lambda : self.changeColor(askcolor()[1]))
-        self.color_selector.grid(column=0, row=0)
+        self.color_selector = Button(self.top_frame, text='Select Color', command= lambda : self.changeColor(askcolor()[1]), highlightthickness = 0, bd = 0, bg = "light gray")
+        self.color_selector.pack(side=LEFT, padx=10)
         self.mostRecentColor = self.DEFAULT_COLOR
+        self.changedColor = False
         image = Image.open("C:/Users/Chris/Documents/Paint/Button_Images/colorselect.jpg")
-        image = image.resize((25, 25), Image.ANTIALIAS)
+        image = image.resize((32, 32), Image.ANTIALIAS)
         colorimg= ImageTk.PhotoImage(image)
         self.color_selector.configure(image=colorimg)
 
  #--------------------tool buttons---------------------
-        self.draw_button = Button(self.top_frame, text="Draw", command = lambda : self.changeColor(self.mostRecentColor))
-        self.draw_button.grid(column=1, row=0)
-        image = Image.open("C:/Users/Chris/Documents/Paint/Button_Images/Pencil.jpg")
-        image = image.resize((25, 25), Image.ANTIALIAS)
+        self.draw_button = Button(self.top_frame, text="Draw", command = lambda : self.changeColor(self.mostRecentColor), highlightthickness = 0, bd = 0, bg = "light gray")
+        self.draw_button.pack(side=LEFT, padx=3)
+        image = Image.open("C:/Users/Chris/Documents/Paint/Button_Images/Pencil.png")
+        image = image.resize((32, 32), Image.ANTIALIAS)
         pencilimg= ImageTk.PhotoImage(image)
         self.draw_button.configure(image=pencilimg)
-        self.eraser_button = Button(self.top_frame, text="Eraser", command = self.setEraser)
-        self.eraser_button.grid(column=2, row=0)
+        self.eraser_button = Button(self.top_frame, text="Eraser", command = self.setEraser, highlightthickness = 0, bd = 0, bg = "light gray")
+        self.eraser_button.pack(side=LEFT, padx=3)
         image = Image.open("C:/Users/Chris/Documents/Paint/Button_Images/Eraser.png")
-        image = image.resize((25, 25), Image.ANTIALIAS)
+        image = image.resize((32, 32), Image.ANTIALIAS)
         eraserimg= ImageTk.PhotoImage(image)
         self.eraser_button.configure(image=eraserimg)
 
 #--------------------clear---------------------
-        self.clear_button = Button(self.top_frame, text="Clear", command = lambda : self.clear())
-        self.clear_button.grid(column=3, row=0)
-        image = Image.open("C:/Users/Chris/Documents/Paint/Button_Images/clear.jpg")
-        image = image.resize((25, 25), Image.ANTIALIAS)
+        self.clear_button = Button(self.top_frame, text="Clear", command = lambda : self.clear(), highlightthickness = 0, bd = 0, bg = "light gray")
+        self.clear_button.pack(side=LEFT, padx=3)
+        image = Image.open("C:/Users/Chris/Documents/Paint/Button_Images/Clear.png")
+        image = image.resize((32, 32), Image.ANTIALIAS)
         clearimg= ImageTk.PhotoImage(image)
         self.clear_button.configure(image=clearimg)
 
  #--------------------grid checkbox---------------------
         self.gridOn = IntVar()
         self.gridOn.set(0)
-        self.show_grid = Checkbutton(self.top_frame, text='Grid', variable=self.gridOn, command = self.checkGrid)
-        self.show_grid.grid(column=5, row=0)
+        self.show_grid = Checkbutton(self.top_frame, text='Grid', variable=self.gridOn, command = self.checkGrid, bg = "light gray")
+        self.show_grid.pack(side=LEFT, padx=10)
 
  #--------------------recent colors---------------------
         self.recent_colors = []
-        for i in range(4):
-            recent_color = Button(self.top_frame,
-                command =lambda: self.selectRecent(self))
-            recent_color.grid(column=6+i, row=0)
-            recent_color
-            self.recent_colors.append(recent_color)
+        recent_color1 = Button(self.top_frame,
+            command =lambda: self.selectRecent(recent_color1), height = 1, width = 2)
+        recent_color1.pack(side=LEFT, padx=5)
+        self.recent_colors.append(recent_color1)
+        recent_color2 = Button(self.top_frame,
+            command =lambda: self.selectRecent(recent_color2), height = 1, width = 2)
+        recent_color2.pack(side=LEFT, padx=5)
+        self.recent_colors.append(recent_color2)
+        recent_color3 = Button(self.top_frame,
+            command =lambda: self.selectRecent(recent_color3), height = 1, width = 2)
+        recent_color3.pack(side=LEFT, padx=5)
+        self.recent_colors.append(recent_color3)
+        recent_color4 = Button(self.top_frame,
+            command =lambda: self.selectRecent(recent_color4), height = 1, width = 2)
+        recent_color4.pack(side=LEFT, padx=5)
+        self.recent_colors.append(recent_color4)
 
  #--------------------tabs---------------------
-        self.tab_control = Notebook(self.window)
+        self.tab_control = Notebook(self.bottom_frame)
         self.tab1 = Frame(self.tab_control)
         self.tab_control.add(self.tab1, text='First')
-        self.tab_control.grid(column = 0, row = 3)
+        self.tab_control.pack(side=LEFT, anchor="nw")
 
 #--------------------canvas---------------------
         self.canvas = bc.BitCanvas()
@@ -115,14 +126,36 @@ class Paint():
         self.eraser = False
 
     def changeColor(self, color):
-
+        #if color selected is already the color, do nothing
+        if(color == self.canvas.getColor()):
+            return
+        #if eraser is on, turn off and change set color
+        if(self.eraser == True):
+            self.eraser = False
+            self.canvas.setColor(color)
+            return
+        if(self.eraser == False and color == self.mostRecentColor):
+            return
+        self.addColortoRecents(self.canvas.getColor())
         self.canvas.setColor(color)
+        self.mostRecentColor = color
 
-    def selectRecent(self, color):
-        self.canvas.setColor(color)
+    def addColortoRecents(self,color):
+        print(color)
+        for i in range(4):
+            if self.recent_colors[i].cget('bg') == color:
+                return
+        self.recent_colors[3].configure(bg = self.recent_colors[2].cget('bg'))
+        self.recent_colors[2].configure(bg = self.recent_colors[1].cget('bg'))
+        self.recent_colors[1].configure(bg = self.recent_colors[0].cget('bg'))
+        self.recent_colors[0].configure(bg = color)
+
+    def selectRecent(self, recentcolor):
+        self.canvas.setColor(recentcolor.cget('bg'))
 
     def setEraser(self):
         self.mostRecentColor = self.canvas.getColor()
+        self.eraser = True
         self.canvas.setColor("White")
 
     def checkGrid(self):
@@ -132,23 +165,35 @@ class Paint():
             self.canvas.removeGrid()
 
     def saveAs(self):
+        ftypes = [('PNG', '*.png'), ('JPEG', '*.jpg'), ('BMP', '*.bmp')]
+        extentionmap = {'.jpg':'JPEG', '.bmp':'BMP', '.png':'PNG'}
+        #get raw image
         bitmap = self.canvas.getImage()
         bitmap.delete('grid')
+        #get filepath from user
         ps = bitmap.postscript(colormode='color')
         img = Image.open(io.BytesIO(ps.encode('utf-8')))
-        self.filepath = asksaveasfilename(initialdir = "C:/Users/Chris/Documents/Paint/images/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-        img.save(self.filepath + ".jpg", 'JPEG')
+        self.filepath = asksaveasfilename(initialdir = "C:/Users/Chris/Documents/Paint/images/", title = "Select file", defaultextension=".*", filetypes = ftypes)
+        #if no path selected, return
+        if(self.filepath == ''):
+            return
+        #save image
+        img.save(self.filepath,extentionmap[self.filepath[-4:]])
         self.filenameGiven = True
+        #reset canvas
         if self.gridOn.get() == 1:
             self.canvas.drawGrid()
 
-    def save(self):
+    def save(self, event=None):
         if(self.filenameGiven):
+            #get raw image
             bitmap = self.canvas.getImage()
             bitmap.delete('grid')
+            #save image
             ps = bitmap.postscript(colormode='color')
             img = Image.open(io.BytesIO(ps.encode('utf-8')))
-            img.save(self.filepath + ".jpg", 'JPEG')
+            img.save(self.filepath + self.fileextension, self.filetype)
+            #reset canvas
             if self.gridOn.get() == 1:
                 self.canvas.drawGrid()
         else:
@@ -156,6 +201,9 @@ class Paint():
 
     def clear(self):
         self.canvas.clear(self.gridOn.get())
+
+    def undo(self, event=None):
+        self.canvas.undo()
 
 
 
